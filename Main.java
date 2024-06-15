11d469740f0a2517ea8c2dd5b3ce08673140b6ed
mighty <mightywolf@users.noreply.github.com>
2024-06-15 11:27:57 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there ");
+        System.out.println("Today's date is : " +new Date());
+    }
+}
